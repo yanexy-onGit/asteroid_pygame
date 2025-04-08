cdaed4b751f98c92bc1f7fd5279ad5c2ae451dd7
nexy <nexyvanessa@gmail.com>
2025-04-08 11:42:33 +0200

    complete main.py to run the full base game

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import constants as C
 import pygame as pg
+from player import Player
+from asteroid import Asteroid
+from bullet import Bullet
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -7,6 +11,15 @@ def main():
     print(f"Screen height: {C.SCREEN_HEIGHT}")
     pg.init()
     screen = pg.display.set_mode((C.SCREEN_WIDTH, C.SCREEN_HEIGHT))
+    drawable, updatable, asteroids, bullets = pg.sprite.Group(), pg.sprite.Group(), pg.sprite.Group(), pg.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (drawable, updatable, asteroids)
+    Bullet.containers = (drawable, updatable, bullets)
+    player = Player(C.SCREEN_WIDTH / 2, C.SCREEN_HEIGHT / 2)
+    AsteroidField()
+    clock = pg.time.Clock()
+    dt = 0
     while True:
         screen.fill("#000000")
 
@@ -14,8 +27,21 @@ def main():
             if e.type == pg.QUIT:
                 return
 
-
+        updatable.update(dt)
+        for aster in asteroids:
+            if player.is_colliding(aster):
+                print("Game over!")
+                return
+            for shot in bullets:
+                if shot.is_colliding(aster):
+                    aster.take_hit()
+                    shot.kill()
+        for member in drawable:
+            member.draw(screen)
+        
         pg.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
